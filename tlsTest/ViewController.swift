55576edc6e134b06f9f0f518de17cdca2ecdafa1
Sam Heather <sam@heather.sh>
2016-03-24 18:26:34 +0000

    Initial Commit

---
+++ tlsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tlsTest
+//
+//  Created by Samuel B Heather on 24/03/2016.
+//  Copyright © 2016 Freedom Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
